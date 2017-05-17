dcd93ac8533eb9cf710f5b5e0b5d1ee83a0fb1be
ageapps <age.apps.dev@gmail.com>
2017-05-17 15:52:33 +0200

    Added hostname in title

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var os = require("os");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
     res.render('index', {
         title: 'SocketIO Chat Demo',
-        number_connected: req.userCount
+        number_connected: req.userCount,
+        host: os.hostname()
     });
 });
 
